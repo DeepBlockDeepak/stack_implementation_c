e058c59431a40be59f2801c13e5518bb40e67bf8
DeepBlockDeepak <jordan.medina1729@gmail.com>
2021-10-16 16:49:25 -0600

    I escaped pointer hell. Created double pointer to initiate the problem solution.

--- linkedlist_stack.c
+++ linkedlist_stack.c
@@ -4,8 +4,7 @@
 #include <limits.h>
 #include <assert.h>
 
-#define STACK_LENGTH 10
-#define EMPTY (-1)
+
 #define STACK_EMPTY INT_MIN
 
 
@@ -16,67 +15,66 @@ struct node_t{
 
 
 
-bool push(struct node_t* stack, int val){
+bool push(struct node_t **stack, int val){
 
     // create the new node and memory check it
     struct node_t* new_node = malloc(sizeof(struct node_t));
     assert(new_node);
 
     new_node->value = val;
-    new_node->next = stack;
+    new_node->next = *stack;
 
-    stack = new_node;
+    *stack = new_node;
+
+    printf("%d is val, %d is stack->value\n", val, (*stack)->value);
     
     return true;
 }
 
-int pop(struct node_t* stack){
+int pop(struct node_t** stack){
 
-    if(!stack){
+    if(!(*stack)){
         return STACK_EMPTY;
     }
 
     
-    struct node_t *tmp = stack; // temporary node for deletion
-    int pop_val = stack->value; // retain the popped value
+    struct node_t *tmp = *stack; // temporary node for deletion
+    int pop_val = (*stack)->value; // retain the popped value
 
-    stack = stack->next; // change head location
+    *stack = (*stack)->next; // change head location
     free(tmp);
 
     return pop_val;
 }
 
-void array_printer(int arr[]){
-    return;
-}
 
-void pop_all(struct node_t* stack){
-    
+void pop_all(struct node_t** stack){
+    printf("\nPopping all nodes off...\n");
     int popped_val;
     while((popped_val = pop(stack)) != STACK_EMPTY){
         printf("%d just got popped.\n", popped_val);
     }
-    printf("Stack popped it all off.\n");
+    //printf("Stack popped it all off.\n");
 }
 
 int main(){
 
 
     struct node_t *stack = NULL;
-    printf("%p == first stack\n", &stack);
-    push(stack, 44);
-    printf("%p == stack outside\n", &stack);
-    
-    //printf("%d is last in.\n", stack->value);
-    push(stack, 55);
-    //printf("%d is last in.\n", stack->value);
-    push(stack, 99);
-    //printf("%d is last in.\n", stack->value);
 
-    /*
-    int popped_val = pop(stack, &top);
-    printf("%d just got popped.\n", popped_val);*/
-    pop_all(stack);
+    push(&stack, 44);
+
+    
+    printf("%d is last in.\n", stack->value);
+    push(&stack, 55);
+    printf("%d is last in.\n", stack->value);
+    push(&stack, 99);
+    printf("%d is last in.\n", stack->value);
+
+    
+    int popped_val = pop(&stack);
+    printf("%d just got popped.bloop\n", popped_val);
+    pop_all(&stack);
 
 
 
